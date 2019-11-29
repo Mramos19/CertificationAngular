94da936557c323abe9412c87b8c2a4e8b819be9a
Martin Ramos <martin.ramos@demetech.us>
2019-11-29 16:28:18 -0600

    Subiendo Proyecto al repositorio

---
+++ src/app/Views/Catalog/bank/bank.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bank',
+  templateUrl: './bank.component.html',
+  styles: []
+})
+export class BankComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Views/Account/login/login.component';
+import { ResetComponent } from './Views/Account/reset/reset.component';
+import { ConfirmComponent } from './Views/Account/confirm/confirm.component';
+import { DefaultComponent } from './Views/Home/default/default.component';
+import { NavbarComponent } from './Views/Shared/navbar/navbar.component';
+import { HeaderComponent } from './Views/Shared/header/header.component';
+import { BreadcrumbsComponent } from './Views/Shared/breadcrumbs/breadcrumbs.component';
+import { FooterComponent } from './Views/Shared/footer/footer.component';
+import { CurrencyComponent } from './Views/Catalog/currency/currency.component';
+import { BankComponent } from './Views/Catalog/bank/bank.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ResetComponent,
+    ConfirmComponent,
+    DefaultComponent,
+    NavbarComponent,
+    HeaderComponent,
+    BreadcrumbsComponent,
+    FooterComponent,
+    CurrencyComponent,
+    BankComponent
   ],
   imports: [
     BrowserModule
